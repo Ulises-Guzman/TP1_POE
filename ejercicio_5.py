9a0a3fe7fc37efa08a0f3e57eaf8424a751bcf37
Ulises <ulisesgroup@gmail.com>
2026-04-21 14:05:03 -0300

    feat(tp): Ejercicio_5 Resuelto

---
+++ ejercicio_5.py
@@ -0,0 +1,31 @@
+﻿print(".: Simulador de Carrito de Compras :.")
+print()
+
+producto = input("Ingrese el nombre del producto: ")
+precio = float(input("Ingrese el precio del producto: $"))
+
+producto_mayor = producto
+precio_mayor = precio
+
+total_gastado = 0.0
+
+while producto.lower() != "fin":
+
+    total_gastado += precio
+
+    if precio > precio_mayor:
+        precio_mayor = precio
+        producto_mayor = producto
+
+    print()
+    producto = input("Ingrese el nombre del producto: ")
+    if producto.lower() != "fin":
+        precio = float(input("Ingrese el precio del producto: $"))
+
+print()
+print(".: Detalle :.")
+print()
+print(f"> El total gastado es : ${total_gastado}")
+print()
+print(f"> El producto más caro es: {producto_mayor} y su precio es: ${precio_mayor}")
+print()
