053aaafc39703907e05d1bf349f9854ae01680a8
Ulises <ulisesgroup@gmail.com>
2026-04-22 13:33:05 -0300

    feat(tp): Ejercicio_6 Resuelto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TP1_POE
+
 Trabajo Práctico de Programación Orientada a Eventos
---
+++ ejercicio_6.py
@@ -0,0 +1,13 @@
+﻿print(".: Coversor de binario a decimal :.")
+print()
+
+numero_binario = input("Ingrese un número binario: ")
+lista_numero_binario = list(numero_binario)
+numero_decimal = 0
+
+for i in reversed(lista_numero_binario):
+    if i == '1':
+        numero_decimal += int(i)**2
+
+print()
+print(f"> Coversión a decimal: {numero_decimal}")
