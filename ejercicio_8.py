f52b150d0a63bcfb379dec934b9db614d4680bfc
Ulises <ulisesgroup@gmail.com>
2026-04-23 09:45:20 -0300

    feat(tp): Ejercicio_8 Resuelto

---
+++ ejercicio_8.py
@@ -0,0 +1,11 @@
+﻿print(".: Análisis de rango de una lista de números:.")
+print()
+
+numeros = [10, 125, 76, 84, 154, 35, 64, 96, 77, 33]
+
+num_max = max(numeros)
+num_min = min(numeros)
+
+resultado = num_max - num_min
+
+print(f"> La diferencia entre el número máximo y mínimo es: {resultado}")
